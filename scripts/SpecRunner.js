cce8f6f8b7a2f9f7bc84169452ccfbc14c1e68ea
Jacob Fierro <jacob.fierro@gmail.com>
2013-05-21 21:50:28 +0100

    set up bootstrap

---
+++ scripts/SpecRunner.js
@@ -0,0 +1,24 @@
+// Configure RequireJS
+require.config({
+  baseUrl:'src/',
+  urlArgs: "v="+(new Date()).getTime()
+});
+
+// Require libraries
+require(['require', 'vendor/chai', 'vendor/mocha'], function(require,chai){
+
+  // Chai
+  assert = chai.assert;
+  should = chai.should();
+  expect = chai.expect;
+
+  // Mocha
+  mocha.setup('bdd');
+
+  // Require base tests before starting
+  require(['spec/context'], function(context){
+    // Start runner
+    mocha.run();
+  });
+
+});
